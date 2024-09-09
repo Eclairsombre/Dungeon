e932e80412f77ad266f72ad5c2e7e8588628e62b
Eclairsombre <alexandre.thny@gmail.com>
2024-09-09 22:07:11 +0200

    Update version number in AssemblyInfo.cs

--- obj/Debug/net8.0/Dungeon.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dungeon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dungeon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3a2e5963bfac8c300d01ed5335869908889d89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a581962bfc05f0ad1f366edecd4cb1dd3b729f4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dungeon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dungeon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
