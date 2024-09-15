b2fb300c6ef43ece8ee9aefb5f845e72d97398a3
Eclairsombre <alexandre.thny@gmail.com>
2024-09-15 21:01:09 +0200

    Refactor logic for enemy class vision

--- src/EnemyClass/Enemy.cs
+++ src/EnemyClass/Enemy.cs
@@ -21,29 +21,30 @@ namespace Dungeon.src.EnemyClass
 
         public float VisionRadius { get; set; } = 150f; // Rayon du champ de vision
         public float VisionAngle { get; set; } = MathHelper.ToRadians(90f); // Angle du champ de vision en radians
+        public float VisionRange { get; set; } = 1000f; // Portée du champ de vision
 
         public Enemy()
         {
             this.width = 50;
             this.height = 50;
-            this.Position = new Vector2(200, 200);
-            this.Direction = new Vector2(1, 0); // Direction initiale (vers la droite)
+            this.Position = new Vector2(300, 200);
+            this.Direction = new Vector2(0, 1); // Direction initiale (vers le bas)
         }
 
         public void Update(Vector2 playerPosition, Room room)
         {
+            this.Position.X += speed;
         }
 
         public void FollowPlayer(Point playerTileIndex, Room room)
         {
             Tiles[,] tiles = room.tiles;
-
+            // Logique pour suivre le joueur
         }
 
-
-        public bool CheckCollisionWithRoom(Tiles[,] tiles, Vector2 potentialPosition)
+        public bool CheckCollisionWithRoom(Tiles[,] tiles)
         {
-            Rectangle enemyHitbox = new Rectangle((int)potentialPosition.X, (int)potentialPosition.Y, width, height);
+            Rectangle enemyHitbox = new Rectangle((int)Position.X, (int)Position.Y, width, height);
 
             for (int i = 0; i < tiles.GetLength(0); i++)
             {
@@ -61,51 +62,66 @@ namespace Dungeon.src.EnemyClass
             return false;
         }
 
+        public bool CheckCollisionVision(Tiles[,] tiles, Vector2 vision)
+        {
+            Rectangle view = new Rectangle((int)vision.X, (int)vision.Y, 1, 1);
 
-
+            for (int i = 0; i < tiles.GetLength(0); i++)
+            {
+                for (int y = 0; y < tiles.GetLength(1); y++)
+                {
+                    if (tiles[i, y].id == 1)
+                    {
+                        if (view.Intersects(tiles[i, y].hitbox))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
         public void Draw(SpriteBatch spriteBatch, Room room)
         {
             Rectangle rect = new Rectangle((int)Position.X, (int)Position.Y, width, height);
             spriteBatch.FillRectangle(rect, Color.Blue);
 
-            // Dessiner le champ de vision
             DrawVision(spriteBatch, room);
         }
 
         public void DrawVision(SpriteBatch spriteBatch, Room room)
         {
-            // Dessiner un cône pour représenter l'angle de vision
+            Vector2 centerPosition = new Vector2(Position.X + width / 2, Position.Y + height / 2);
+
             Vector2 leftBoundary = Vector2.Transform(Direction, Matrix.CreateRotationZ(-VisionAngle / 2));
             Vector2 rightBoundary = Vector2.Transform(Direction, Matrix.CreateRotationZ(VisionAngle / 2));
 
-            List<Vector2> visionVertices = new List<Vector2> { Position };
-            visionVertices.Add(Position + leftBoundary * VisionRadius);
-            visionVertices.Add(Position + rightBoundary * VisionRadius);
+            Vector2 leftPoint = centerPosition + leftBoundary * VisionRange;
+            Vector2 rightPoint = centerPosition + rightBoundary * VisionRange;
 
-            // Vérifier les collisions avec les murs pour bloquer la vision
-            for (int i = 0; i < visionVertices.Count - 1; i++)
+            List<Vector2> visionVertices = new List<Vector2> { centerPosition, leftPoint, rightPoint };
+
+            for (int i = 1; i < visionVertices.Count; i++)
             {
-                Vector2 start = visionVertices[i];
-                Vector2 end = visionVertices[i + 1];
+                Vector2 start = visionVertices[0];
+                Vector2 end = visionVertices[i];
                 Vector2 direction = end - start;
                 direction.Normalize();
 
-                for (float j = 0; j < VisionRadius; j += 1f)
+                for (float j = 0; j < VisionRange; j += 1f)
                 {
                     Vector2 checkPosition = start + direction * j;
-                    if (CheckCollisionWithRoom(room.tiles, checkPosition))
+                    if (CheckCollisionVision(room.tiles, checkPosition))
                     {
-                        visionVertices[i + 1] = checkPosition;
+                        visionVertices[i] = checkPosition;
                         break;
                     }
                 }
             }
 
-            // Dessiner le cône de vision
-            for (int i = 0; i < visionVertices.Count - 1; i++)
-            {
-                spriteBatch.DrawLine(visionVertices[i], visionVertices[i + 1], Color.Yellow);
-            }
+            spriteBatch.DrawLine(visionVertices[0], visionVertices[1], Color.Yellow);
+            spriteBatch.DrawLine(visionVertices[0], visionVertices[2], Color.Yellow);
+            spriteBatch.DrawLine(visionVertices[1], visionVertices[2], Color.Yellow * 0.5f);
         }
     }
 }
