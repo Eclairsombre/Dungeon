ce378f118dd127638d5df4b0a40a4a51e9215245
Eclairsombre <alexandre.thny@gmail.com>
2024-09-24 23:15:10 +0200

    add aseprite doc

--- obj/Debug/net8.0/Dungeon.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dungeon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dungeon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7314232eb85ae752a7e7f1bdf1339aea4f777c6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d56e6de7c61cbc1d014c2f1b84f5c59bbe5896")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dungeon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dungeon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
